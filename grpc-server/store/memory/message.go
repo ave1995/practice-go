43c1611a0d5bb15f01f44a1cf8565f9b2c175729
ave <veselya95@gmail.com>
2025-10-29 19:00:14 +0100

    Second possibility how to implement cache

---
+++ grpc-server/store/memory/message.go
@@ -0,0 +1,65 @@
+package memory
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/ave1995/practise-go/grpc-server/domain/model"
+	"github.com/ave1995/practise-go/grpc-server/domain/store"
+)
+
+var _ store.MessageStore = (*MessageStore)(nil)
+
+type MessageStore struct {
+	messageStore store.MessageStore
+	mu           sync.RWMutex
+	items        map[model.MessageID]*model.Message
+}
+
+func NewMessageStore(store store.MessageStore) *MessageStore {
+	return &MessageStore{
+		messageStore: store,
+		items:        make(map[model.MessageID]*model.Message),
+	}
+}
+
+func (m *MessageStore) Fetch(ctx context.Context, id model.MessageID) (*model.Message, error) {
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	m.mu.RLock()
+	item, found := m.items[id]
+	m.mu.RUnlock()
+
+	if found {
+		return item, nil
+	}
+
+	msg, err := m.messageStore.Fetch(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch message: %w", err)
+	}
+
+	m.mu.Lock()
+	m.items[id] = msg
+	m.mu.Unlock()
+
+	return msg, nil
+}
+
+func (m *MessageStore) Create(ctx context.Context, text string) (*model.Message, error) {
+	msg, err := m.messageStore.Create(ctx, text)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create message: %w", err)
+	}
+
+	m.mu.Lock()
+	m.items[msg.ID] = msg
+	m.mu.Unlock()
+
+	return msg, nil
+}
---
+++ grpc-server/store/redis/message.go
@@ -0,0 +1,89 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/ave1995/practise-go/grpc-server/domain/model"
+	"github.com/ave1995/practise-go/grpc-server/domain/store"
+	"github.com/ave1995/practise-go/grpc-server/utils"
+	"github.com/redis/go-redis/v9"
+)
+
+var _ store.MessageStore = (*MessageStore)(nil)
+
+type MessageStore struct {
+	logger       *slog.Logger
+	messageStore store.MessageStore
+	redisClient  *redis.Client
+}
+
+func NewMessageStore(logger *slog.Logger, store store.MessageStore, addr string, password string, db int) *MessageStore {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	return &MessageStore{
+		logger:       logger,
+		messageStore: store,
+		redisClient:  rdb,
+	}
+}
+
+func (m *MessageStore) Fetch(ctx context.Context, id model.MessageID) (*model.Message, error) {
+	key := id.String()
+
+	data, err := m.redisClient.Get(ctx, key).Bytes()
+	if err == nil {
+		var msg model.Message
+		if err := json.Unmarshal(data, &msg); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal cached message: %w", err)
+		}
+		return &msg, nil
+	}
+
+	if !errors.Is(err, redis.Nil) {
+		return nil, fmt.Errorf("failed to get message from redis: %w", err)
+	}
+
+	msg, err := m.messageStore.Fetch(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch message from store: %w", err)
+	}
+
+	data, err = json.Marshal(msg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	if err := m.redisClient.Set(ctx, key, data, 0).Err(); err != nil {
+		m.logger.Warn("failed to set redis cache", utils.SlogError(err))
+	}
+
+	return msg, nil
+}
+
+func (m *MessageStore) Create(ctx context.Context, text string) (*model.Message, error) {
+	msg, err := m.messageStore.Create(ctx, text)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create message: %w", err)
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	key := msg.ID.String()
+
+	if err := m.redisClient.Set(ctx, key, data, 0).Err(); err != nil {
+		m.logger.Warn("failed to set redis cache", utils.SlogError(err))
+	}
+
+	return msg, nil
+}
